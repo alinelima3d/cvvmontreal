49e076ecbe7c4d051b60e2481a46cbe1d1e552b6
alinelima3d <aline3d@gmail.com>
2025-06-11 14:57:30 -0400

    fix database

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my super secret key that no one is suppose to know'
-if app.debug:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/cvv'
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ue9r2df1jcvfh2:p77d7215e49ae55f05cdd9aeceb8bbd3d601d18cd9fba706bac3c09e836b2575d@c2hbg00ac72j9d.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6igtkis6hsviv'
+# if app.debug:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/cvv'
+# else:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ue9r2df1jcvfh2:p77d7215e49ae55f05cdd9aeceb8bbd3d601d18cd9fba706bac3c09e836b2575d@c2hbg00ac72j9d.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6igtkis6hsviv'
 db = SQLAlchemy(app)
 
 
